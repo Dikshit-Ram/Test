a31e62695c714e87d3a1f5589daa2ac86d9b772f
dikshit ram <dikshit.ram02@gmail.com>
2017-06-23 12:16:49 -0400

    test import

--- src/main/java/io/egen/training/entity/Vehicle.java
+++ src/main/java/io/egen/training/entity/Vehicle.java
@@ -3,9 +3,7 @@ package io.egen.training.entity;
 import java.util.Date;
 import java.util.List;
 
-/**
- * Created by diksh on 6/23/2017.
- */
+
 public class Vehicle {
     private String vin;
     private String make;
