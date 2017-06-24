8017a6d44af7a726146dc5b5de5c515325b42fa3
shiva2157k <shivak916@gmail.com>
2017-06-23 20:23:54 -0400

    Added Attributes

--- src/main/java/io/egen/training/entity/Alerts.java
+++ src/main/java/io/egen/training/entity/Alerts.java
@@ -1,11 +1,15 @@
 package io.egen.training.entity;
 
 
+import java.util.Date;
+
 public class Alerts {
     private enum Alert{
         HIGH, MEDIUM, LOW
     }
 
+    private String vin;
+    private Date timestamp;
     private Alert engineRpmAlert;
     private Alert fuelVolumeAlert;
     private Alert tirePressureAlert;
