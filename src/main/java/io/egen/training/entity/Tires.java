8e842d5a0a3455231ce768d6cd711812d899186c
Unknown <dikshit.ram02@gmail.com>
2017-06-26 12:42:01 -0400

    feat(module-api): add alerts repo and controller methods

---
+++ src/main/java/io/egen/training/entity/Tires.java
@@ -0,0 +1,14 @@
+package io.egen.training.entity;
+
+import lombok.Data;
+
+/**
+ * Created by diksh on 6/26/2017.
+ */
+@Data
+public class Tires{
+    private byte frontLeft;
+    private byte frontRight;
+    private byte rearLeft;
+    private byte rearRight;
+}
